53c7b4caf5707ca835b4f8878bf0b4cd44aa769f
Presh Onyee <presh_onyee@outlook.com>
2020-08-22 09:49:01 +0100

    feat: add new docs & modify styling
    This change introduces some minor style and layout changes.
    On desktop, I want the layout to have enough large margins on both ends.
    
    I further reduced the font-sizes of the main content area and the table of contents.
    
    I've also added new introduction docs.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,35 +1,33 @@
-import { DokzProvider, GithubLink, ColorModeSwitch } from 'dokz'
-import React, { Fragment } from 'react'
-import Head from 'next/head'
+import { DokzProvider, GithubLink, ColorModeSwitch } from "dokz";
+import React, { Fragment } from "react";
+import Head from "next/head";
+import "../styles.css";
 
 export default function App(props) {
-    const { Component, pageProps } = props
-    return (
-        <Fragment>
-            <Head>
-                <link
-                    href='https://fonts.googleapis.com/css?family=Fira+Code'
-                    rel='stylesheet'
-                    key='google-font-Fira'
-                />
-            </Head>
-            <DokzProvider
-                headerItems={[
-                    <GithubLink
-                        key='0'
-                        url='https://github.com/remorses/dokz'
-                    />,
-                    <ColorModeSwitch key='1' />,
-                ]}
-                sidebarOrdering={{
-                    'index.mdx': true,
-                    Documents_Group: {
-                        'another.mdx': true,
-                    },
-                }}
-            >
-                <Component {...pageProps} />
-            </DokzProvider>
-        </Fragment>
-    )
+  const { Component, pageProps } = props;
+  return (
+    <div style={{ width: "70%", margin: "0 auto" }}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Fira+Code"
+          rel="stylesheet"
+          key="google-font-Fira"
+        />
+      </Head>
+      <DokzProvider
+        headerItems={[
+          <GithubLink key="0" url="https://github.com/remorses/dokz" />,
+          <ColorModeSwitch key="1" />,
+        ]}
+        sidebarOrdering={{
+          "index.mdx": true,
+          Documents_Group: {
+            "another.mdx": true,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </DokzProvider>
+    </div>
+  );
 }
